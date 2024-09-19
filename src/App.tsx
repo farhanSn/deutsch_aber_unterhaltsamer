f6c0bb27be5dcf37074755c2397513ed9ca355c4
farhan.1 <farhank1376@gmail.com>
2024-09-19 13:13:45 +0330

    make contest

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
     const [data, setData] = useState<any>([]);
     const [loading, setLoading] = useState(true);
     const [index, setIndex] = useState(0);
+    const [choices, setChoices] = useState([0]);
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,32 +34,56 @@ function App() {
 
     }, []);
 
+    const createChoices= () => {
+        setChoices([]);
+        let arr = [index];
+        for(let i = index; i < index+100; i++){
+            if(arr.find((a)=> a === (i * 2 + 3))){
+                continue;
+            }
+            const newChoice = (i * 2 + 3);
+            arr = [...arr, newChoice];
+            if(arr.length === 4){
+                setChoices(arr.sort(() => 0.5 - Math.random()));
+                break;
+            }
+        }
+    }
+    useEffect(() => {
+        createChoices();
+    }, [index]);
+
+    const buttonBackground = (i: number) => {
+        if(showAnswer && i === index){
+            return "lightseagreen"
+        }
+        if(showAnswer && i === selectedIndex){
+            return "red"
+        }
+    }
 
 
   return loading ? <div>loading...</div> : (
       <div style={{margin:0, paddingTop:24,height: '100vh', display: "flex", flexDirection: 'column', paddingInline:16}}>
           <Card title={data[index].word} bordered={false} style={{ padding: 16, marginTop:100, boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}>
-              <p>{data[index].def}</p>
+              <Flex style={{ gap: 6, marginTop: 'auto', marginBottom: 60, flexDirection: "column"}}>
+                  {choices.map((i)=> <Button disabled={showAnswer} style={{flex:1, paddingBlock: 24, backgroundColor: buttonBackground(i)}} onClick={()=> {
+                      setShowAnswer(true);
+                      setSelectedIndex(i);
+                      setTimeout(()=> {
+                          setIndex((ii)=> (ii+1) % data.length);
+                          setShowAnswer(false);
+                      }, 2000);
+
+
+                  }}>
+                      <Typography style={{fontWeight: 600, textWrap: "wrap"}}>
+                          {data[i].def}
+                      </Typography>
+                  </Button>)}
+              </Flex>
           </Card>
           <Flex style={{ gap: 6, marginTop: 'auto', marginBottom: 60, flexDirection: "column"}} >
-              <Flex style={{justifyContent: "space-around", gap: 24, flexDirection: "row"}}>
-                  <Button style={{flex:1, paddingBlock: 24}} onClick={()=> setIndex((i)=> {
-                      if(i-1<0){
-                          console.log(data.length -1)
-                          return data.length -1;
-                      }
-                      return i-1
-                  })}>
-                      <Typography style={{fontWeight: 600}}>
-                          قبلی
-                      </Typography>
-                  </Button>
-                  <Button  style={{flex:1, paddingBlock: 24}} onClick={()=> setIndex((i)=> (i+1) % data.length)}>
-                      <Typography style={{fontWeight: 600}}>
-                          بعدی
-                      </Typography>
-                  </Button>
-              </Flex>
               <Flex style={{justifyContent: "center", gap: 6, flexDirection: "row"}} >
                   <Typography style={{fontWeight: 600, fontSize:20}}>
                       {index+1}
