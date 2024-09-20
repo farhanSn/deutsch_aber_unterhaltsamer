1dd02e5af1b5bbfb431e9552a0109991aab4c4cb
farhan.1 <farhank1376@gmail.com>
2024-09-21 03:08:13 +0330

    ??

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={'/deutsch_aber_unterhaltsamer'}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
