27261fd3f5ad632b738fa4dcdd0e791f3b6be2b2
farhan.1 <farhank1376@gmail.com>
2024-09-21 15:23:32 +0330

    fix

--- src/pages/global/index.tsx
+++ src/pages/global/index.tsx
@@ -1,22 +1,21 @@
 /* eslint no-use-before-define: 0 */  // --> OFF
 import {Button} from "antd";
-import { NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 
 const Global = () => {
+
+    const navigate = useNavigate();
+
     return <div style={{margin:0, paddingTop:24,height: '100vh', display: "flex", flexDirection: 'column', paddingInline:16, gap:16, justifyContent: 'center'}}>
-        <Button onClick={()=> {}}>
+        <Button onClick={()=> navigate('b1')}>
             <p>
-                <NavLink style={{width: '100%', height: '100%', color: 'black'}} to={'/b1'}>
                     Starten Wir B1
-                </NavLink>
             </p>
         </Button>
-        <Button onClick={()=> {}}>
+        <Button onClick={()=> navigate('a2')}>
             <p>
-                <NavLink style={{width: '100%', height: '100%', color: 'black'}} to={'/a2'}>
                     Starten Wir A2
-                </NavLink>
             </p>
         </Button>
         </div>
